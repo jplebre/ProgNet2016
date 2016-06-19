c84d9e7463af148832ef50a103eb731e44e1190d
dylanbeattie <dylan@dylanbeattie.net>
2016-06-19 12:29:06 +0100

    Added DelayWeb web project to create configurable endpoint used in async demos.

---
+++ DelayWeb/Modules/IndexModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using Nancy;
+
+namespace DelayWeb.Modules {
+    public class IndexModule : NancyModule {
+        public IndexModule() {
+            Get["/hello/{name?World}"] = parameters => {
+                int delay;
+                Int32.TryParse(Request.Query["delay"], out delay);
+                if (delay > 0) Thread.Sleep(TimeSpan.FromSeconds(delay));
+                return ("Hello, " + parameters["name"]);
+            };
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+Code for @dylanbeattie's Asynchronous Programming Workshop
+==========================================================
+
+Progressive.NET Tutorials 2016
+------------------------------
+
