92283c6f6ff071f9787848d8961f70908119b2d9
Dylan Beattie <dylan@dylanbeattie.net>
2016-06-22 13:29:04 +0100

    Final tweaks before workshop. Here we go :)

--- KeyQuest/Modules/IndexModule.cs
+++ KeyQuest/Modules/IndexModule.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Threading;
 using Nancy;
 
@@ -12,6 +13,13 @@ namespace KeyQuest.Modules {
                 Int32.TryParse(Request.Query["delay"], out delay);
                 if (delay > 0) Thread.Sleep(TimeSpan.FromSeconds(delay));
                 return ("Hello, " + parameters["name"]);
+            };
+
+            Get["/chuck"] = _ => {
+                Thread.Sleep(1000);
+                using (var wc = new WebClient()) {
+                    return (wc.DownloadString("http://api.icndb.com/jokes/random"));
+                }
             };
         }
     }
